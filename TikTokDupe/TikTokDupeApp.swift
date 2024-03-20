1650d8b47cded126aa57f942974a9c196c1d043e
Richard Childs <ChildsRichard83@gmail.com>
2024-03-20 02:37:44 -0700

    Initial Commit

---
+++ TikTokDupe/TikTokDupeApp.swift
@@ -0,0 +1,17 @@
+//
+//  TikTokDupeApp.swift
+//  TikTokDupe
+//
+//  Created by Richard Childs on 3/20/24.
+//
+
+import SwiftUI
+
+@main
+struct TikTokDupeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
